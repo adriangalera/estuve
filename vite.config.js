13ab48923086e3dc2fed1238bf00c80d6defe5eb
Adrian Galera <adrian.galera.87@gmail.com>
2025-03-12 20:58:01 +0100

    vite setup

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+    root: ".",  // Ensure Vite serves from root
+    build: {
+      outDir: "dist", // Output folder
+      rollupOptions: {
+        input: "index.html", // Entry point
+      },
+    },
+  };
+  
