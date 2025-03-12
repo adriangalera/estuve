23cee1b2d16209575407d1e2e264074a874b7a48
Adrian Galera <adrian.galera.87@gmail.com>
2025-03-12 21:39:32 +0100

    Serve every library from local

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
-console.log("Hello, world!");
-document.body.appendChild(document.createTextNode("JS is working!"));
+const map = L.map("map", {
+    center: [41.53289317099601, 2.104000992549118],
+    zoom: 14
+});
+
+const osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    zIndex: 1,
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+});
+
+osm.addTo(map);
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 export default {
-    root: ".",  // Ensure Vite serves from root
-    build: {
-      outDir: "dist", // Output folder
-      rollupOptions: {
-        input: "index.html", // Entry point
-      },
+  root: ".",  // Ensure Vite serves from root
+  build: {
+    outDir: "dist", // Output folder
+    rollupOptions: {
+      input: "index.html", // Entry point
     },
-  };
-  
+  },
+  server: {
+    allowedHosts: ["galblog"]
+  }
+};
