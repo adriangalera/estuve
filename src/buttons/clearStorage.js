26e258d51e18b5156eb15af8f6ab7c00ab4b8b5f
Adrian Galera <adrian.galera.87@gmail.com>
2025-03-16 17:27:46 +0100

    Fix clear async

--- src/buttons/clearStorage.js
+++ src/buttons/clearStorage.js
@@ -4,9 +4,9 @@ import 'leaflet-easybutton';
 export const addClearStorageButton = (map, fileLoadStorage, qtStorage, quadtree) => {
     L.easyButton('fa-trash', function (btn, map) {
         fileLoadStorage.clear()
-        qtStorage.clear()
-        quadtree.clear()
-        document.dispatchEvent(new CustomEvent('mapUpdate', {detail: {qt: quadtree}}));
-        location.reload()
+        qtStorage.clear().then( () => {
+            quadtree.clear()
+            document.dispatchEvent(new CustomEvent('mapUpdate', {detail: {qt: quadtree}}));
+        } )
     }).addTo(map);
 }
