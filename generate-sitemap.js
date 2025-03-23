b49e2ec1b2bca8d26bf8a9c674e85ce57552622e
Adrian Galera <adrian.galera.87@gmail.com>
2025-03-23 11:02:09 +0100

    robots.txt and sitemap.xml

---
+++ generate-sitemap.js
@@ -0,0 +1,18 @@
+// generate-sitemap.js
+const fs = require("fs");
+
+const baseUrl = "https://estuve.eu";
+const lastmod = new Date().toISOString().split("T")[0];
+
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${baseUrl}/</loc>
+    <lastmod>${lastmod}</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+fs.writeFileSync("dist/sitemap.xml", sitemap);
+console.log("✅ sitemap.xml generated");
