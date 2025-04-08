b1f7dd6ea92f6eb5b300a81c7178a23a0d2e73dc
Adrian Galera <adrian.galera.87@gmail.com>
2025-04-08 14:24:10 +0200

    Remove conflicting test

--- test/ui/download-button.cy.js
+++ test/ui/download-button.cy.js
@@ -10,19 +10,6 @@ describe("Download button", () => {
         cy.noPointsAdded();
     })
 
-    it("Downloads quadtree", () => {
-        cy.get('input[type="file"]').selectFile(
-            "gpx/out-of-sync/0-27-10-20-la-mola-318-m-100-cims-bonastre-tarragona.gpx",
-            { force: true });
-
-        cy.shouldHaveSomePointsAdded()
-        cy.get(".fa-download").click()
-        const downloadsFolder = Cypress.config("downloadsFolder");
-        cy.readFile(path.join(downloadsFolder, "estuve.bin"), 'binary')
-            .should("exist")
-            .should('have.length.gt', emptyBackupFile)
-    })
-
     it("Download empty quadtree", () => {
         cy.get(".fa-download").click()
         const downloadsFolder = Cypress.config("downloadsFolder");
